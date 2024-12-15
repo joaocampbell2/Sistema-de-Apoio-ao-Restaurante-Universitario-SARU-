9c455f328c3c26c9207ca02085e22d7f51229c67
MariaCBarboza <Maria.clara.ferreira1026@gmail.com>
2024-12-15 20:03:59 -0300

    HOME

--- saru-web/src/app/app.routes.ts
+++ saru-web/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component'; 
+import { NavbarComponent } from './components/navbar/navbar.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: 'home',
+        component:HomeComponent,
+        children:[{
+            path:"",
+            component: NavbarComponent
+        }]
+    },
+
+    {
+        path: "**",
+        redirectTo:'home'
+    }
+
+];
