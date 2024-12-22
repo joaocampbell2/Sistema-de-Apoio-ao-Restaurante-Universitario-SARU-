25ee24025df5bebc90ef3dd3d93652daafc14f21
Joao Campbell <jpcampbell2012@hotmail.com>
2024-12-22 18:35:52 -0300

    feat: saru-rest

---
+++ saru-rest/src/main/java/saru/saru_rest/SaruRestApplication.java
@@ -0,0 +1,13 @@
+package saru.saru_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaruRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaruRestApplication.class, args);
+	}
+
+}
---
+++ saru-rest/src/test/java/saru/saru_rest/SaruRestApplicationTests.java
@@ -0,0 +1,13 @@
+package saru.saru_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaruRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
