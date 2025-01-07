cf2c57bd75adc78bc7523bb71cd8bce386263002
Joao Campbell <jpcampbell2012@hotmail.com>
2025-01-06 23:04:32 -0300

    feat: resposta da response e construtor do controller

--- saru-rest/src/main/java/saru/saru_rest/controller/AdicionarSaldoController.java
+++ saru-rest/src/main/java/saru/saru_rest/controller/AdicionarSaldoController.java
@@ -1,14 +1,12 @@
 package saru.saru_rest.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.*;
 
 import jakarta.annotation.security.DeclareRoles;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.persistence.PostUpdate;
+import saru.saru_rest.dtos.SaldoDTO;
 import saru.saru_rest.service.AdicionarSaldoService;
 import java.lang.Object;
 import saru.saru_rest.security.JwtService;
@@ -16,13 +14,19 @@ import saru.saru_rest.security.JwtService;
 
 @DeclareRoles({"ALUNO, PROFESSOR"})
 @RolesAllowed({"ALUNO, PROFESSOR"})
+@RestController
 public class AdicionarSaldoController {
     private AdicionarSaldoService adicionarSaldoService;
     private JwtService jwtService;
 
+    public AdicionarSaldoController(AdicionarSaldoService adicionarSaldoService, JwtService jwtService) {
+        this.adicionarSaldoService = adicionarSaldoService;
+        this.jwtService = jwtService;
+    }
+
     @PutMapping(value="/adicionarSaldo")
-    public ResponseEntity<String> adicionarSaldo(@RequestHeader(value="Authorization")String Jw, @RequestBody float valor){
-        adicionarSaldoService.adicionarSaldo(jwtService.pegarCpfDoToken(Jw), valor);
-        return ResponseEntity.ok().body(null);
+    public ResponseEntity<String> adicionarSaldo(@RequestHeader(value="Authorization")String Jw, @RequestBody SaldoDTO valor){
+        adicionarSaldoService.adicionarSaldo(jwtService.pegarCpfDoToken(Jw), valor.getValor());
+        return ResponseEntity.ok().body("Saldo Adicionado com sucesso10");
     }
 }
