0dcbbf64335b3ef569e1bdb77deaff1ea7e0b4e2
Bernardo Medeiros <bernmedman@gmail.com>
2025-01-08 20:12:35 -0300

    fix: retorno de mensagem de erro do adicionarSaldo

--- saru-rest/src/main/java/saru/saru_rest/controller/AdicionarSaldoController.java
+++ saru-rest/src/main/java/saru/saru_rest/controller/AdicionarSaldoController.java
@@ -1,12 +1,14 @@
 package saru.saru_rest.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import jakarta.annotation.security.DeclareRoles;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.persistence.PostUpdate;
 import saru.saru_rest.dtos.SaldoDTO;
+import saru.saru_rest.security.SecurityConfig;
 import saru.saru_rest.service.AdicionarSaldoService;
 import java.lang.Object;
 import saru.saru_rest.security.JwtService;
@@ -26,7 +28,6 @@ public class AdicionarSaldoController {
 
     @PutMapping(value="/adicionarSaldo")
     public ResponseEntity<String> adicionarSaldo(@RequestHeader(value="Authorization")String Jw, @RequestBody SaldoDTO valor){
-        adicionarSaldoService.adicionarSaldo(jwtService.pegarCpfDoToken(Jw), valor.getValor());
-        return ResponseEntity.ok().body("Saldo Adicionado com sucesso10");
+        return ResponseEntity.ok().body(adicionarSaldoService.adicionarSaldo((String) SecurityContextHolder.getContext().getAuthentication().getPrincipal(), valor.getValor()));
     }
 }
