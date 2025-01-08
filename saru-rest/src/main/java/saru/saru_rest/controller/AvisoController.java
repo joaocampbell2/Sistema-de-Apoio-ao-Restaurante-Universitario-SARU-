56925ea22aac4c9bba9aed29a8151d124c2fec7a
Bernardo Medeiros <bernmedman@gmail.com>
2025-01-08 00:25:34 -0300

    Issue#12 fix controller para receber metodo de publicar avisos

--- saru-rest/src/main/java/saru/saru_rest/controller/AvisoController.java
+++ saru-rest/src/main/java/saru/saru_rest/controller/AvisoController.java
@@ -1,10 +1,12 @@
 package saru.saru_rest.controller;
 
 
+import jakarta.annotation.security.DeclareRoles;
+import jakarta.annotation.security.RolesAllowed;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 import saru.saru_rest.dtos.AvisoDTO;
 import saru.saru_rest.entity.AvisoEntity;
 import saru.saru_rest.service.AvisoService;
@@ -12,14 +14,23 @@ import saru.saru_rest.service.AvisoService;
 import java.util.List;
 
 @RestController
+@DeclareRoles({"FUNCIONARIO"})
+@RolesAllowed({"FUNCIONARIO"})
+@RequestMapping(value = "/aviso")
 public class AvisoController {
 
     @Autowired
     private AvisoService avisoService;
 
-    @GetMapping(value = "/avisos")
+    @GetMapping(value = "/buscarAvisos")
     public ResponseEntity<List<AvisoDTO>> listarAvisos() {
         List<AvisoDTO> avisos = avisoService.buscarTodosAvisos();
         return ResponseEntity.ok(avisos);
+    }
+
+    @PostMapping(value = "/publicarAvisos")
+    public ResponseEntity<String> publicarAviso(@RequestBody AvisoDTO aviso) {
+        String cpf = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return ResponseEntity.ok(avisoService.publicarAviso(cpf, aviso));
     }
 }
