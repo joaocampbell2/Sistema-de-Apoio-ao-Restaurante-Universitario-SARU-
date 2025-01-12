ae38b996fe6fffe5c1229a88e0e9ed15494021ba
Joao Campbell <jpcampbell2012@hotmail.com>
2025-01-12 17:52:05 -0300

    feat: component criar refeicao

---
+++ saru-web/src/app/components/comprar-refeicao/comprar-refeicao.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comprar-refeicao',
+  imports: [],
+  templateUrl: './comprar-refeicao.component.html',
+  styleUrl: './comprar-refeicao.component.scss',
+})
+export class ComprarRefeicaoComponent { }
