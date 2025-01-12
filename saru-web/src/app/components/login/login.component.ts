3125bff7231c3afa9b90fa4af555f05dc2cccc4c
Joao Campbell <jpcampbell2012@hotmail.com>
2025-01-12 19:22:08 -0300

    feat: tela login inicio

--- saru-web/src/app/components/login/login.component.ts
+++ saru-web/src/app/components/login/login.component.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent { }
+export class LoginComponent {
+
+  form: FormGroup;
+
+  constructor(){
+
+    this.form = new FormGroup(
+      {
+        cpf: new FormControl("",[Validators.required, Validators.maxLength(11),Validators.minLength(11)]),
+        senha: new FormControl("",[Validators.required])
+      }
+    )
+  }
+
+  onSubmit(){
+
+    if(this.form.valid){
+      console.log(this.form.value)
+    }
+    else{
+      console.log("nao")
+    }
+  }
+
+
+ }
