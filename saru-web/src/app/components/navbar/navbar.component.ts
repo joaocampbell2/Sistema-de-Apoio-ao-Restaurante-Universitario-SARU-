021d20da56e69ae35981e448178b8e9a7cfab28a
Bernardo <bernmedman@gmail.com>
2025-01-13 17:48:13 -0300

    fix: reimplementação navbar

--- saru-web/src/app/components/navbar/navbar.component.ts
+++ saru-web/src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import {  Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-navbar',
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent { }
+
+export class NavbarComponent {
+  private httpClient;
+  private saldo:any;
+  constructor(httpClient: HttpClient){
+    this.httpClient = httpClient;
+    this.saldo = 0;
+  }
+
+  onInit(){
+    this.httpClient.get("http//:localhost8080/cliente/resgatarDados").subscribe(response => {
+      this.saldo = response
+    })
+  }
+
+ }
