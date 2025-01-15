00656e7ee15780444a0074a43189e29ef17bfe31
caioabcoutinho <caioabcoutinho@hotmail.com>
2025-01-15 17:05:27 -0300

    montei uma parte do componente cadastro

--- saru-web/src/app/components/cadastrar/cadastrar.component.ts
+++ saru-web/src/app/components/cadastrar/cadastrar.component.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cadastrar',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './cadastrar.component.html',
-  styleUrl: './cadastrar.component.scss',
+  styleUrls: ['./cadastrar.component.scss'],
 })
-export class CadastrarComponent { }
+export class CadastrarComponent {
+  cadastroForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.cadastroForm = this.fb.group({
+      nome: ['', [Validators.required]],
+      cpf: ['', [Validators.required, Validators.minLength(11), Validators.maxLength(11)]],
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.cadastroForm.valid) {
+      console.log('Formulário enviado:', this.cadastroForm.value);
+      //adicionar a lógica de envio ao back-end
+    } else {
+      console.error('Formulário inválido');
+    }
+  }
+}
