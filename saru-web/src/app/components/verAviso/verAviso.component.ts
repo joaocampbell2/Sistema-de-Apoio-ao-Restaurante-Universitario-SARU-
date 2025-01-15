c532311e9228466ecdf611f56e54722c1f339fdb
Bernardo Medeiros <bernmedman@gmail.com>
2025-01-15 19:13:53 -0300

    feat: atualizações para ler atributos dos avisos

--- saru-web/src/app/components/verAviso/verAviso.component.ts
+++ saru-web/src/app/components/verAviso/verAviso.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { CommonModule } from '@angular/common';
+import { AvisoDTO } from '../../models/AvisoDTO';
 
 @Component({
   selector: 'app-verAvisos',
@@ -17,9 +18,10 @@ export class VerAvisosComponent {
     }
   
     ngOnInit(){
-      this.httpClient.get("http://localhost:8080/avisos/buscarAvisos", {headers: {"Access-Control-Allow-Origin": "*","Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyOTU4NDg5NDY1NSIsInJvbGUiOiJBTFVOTyJ9.uK-JLH7gjKalUhowcpeg-_dkVQDrC5hkQyrZKI6lS2E"}}).subscribe(response => {
+      this.httpClient.get<Array<AvisoDTO>>("http://localhost:8080/avisos/buscarAvisos", {headers: {"Access-Control-Allow-Origin": "*","Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyOTU4NDg5NDY1NSIsInJvbGUiOiJBTFVOTyJ9.uK-JLH7gjKalUhowcpeg-_dkVQDrC5hkQyrZKI6lS2E"}}).subscribe(response => {
         console.log(response)
         this.avisos = response;
+        
       })
     }
  }
