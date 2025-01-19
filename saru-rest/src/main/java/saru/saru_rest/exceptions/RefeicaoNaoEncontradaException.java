cdd3fa14441a2dc89254ea82c26572e64d379182
Joao Campbell <jpcampbell2012@hotmail.com>
2025-01-19 17:33:48 -0300

    feat: filter ignorar auth e exceptions

--- saru-rest/src/main/java/saru/saru_rest/exceptions/RefeicaoNaoEncontradaException.java
+++ saru-rest/src/main/java/saru/saru_rest/exceptions/RefeicaoNaoEncontradaException.java
@@ -1,7 +1,7 @@
 package saru.saru_rest.exceptions;
 
 public class RefeicaoNaoEncontradaException extends RuntimeException {
-  public RefeicaoNaoEncontradaException(String message) {
-    super(message);
-  }
+    public RefeicaoNaoEncontradaException() {
+        super();
+    }
 }
